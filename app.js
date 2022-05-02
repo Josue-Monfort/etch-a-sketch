816c74358b2f03f299299d75f102cb5e46d70787
josue <josuemonfortsantiago@gmail.com>
2022-05-02 18:59:05 -0300

    Added the option for the user to input a canvas size

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
+const btnChangeSize = document.querySelector(".newSize"); // Button to change size
+const canvasSizeInput = document.querySelector(".canvasSizeInput"); // Gets the text input
+const messageAlert = document.querySelector("#alertMessage");
+
 function makeCanvas(pixelSize) {
     const canvasDisplay = document.querySelector(".canvasContainer");
+    const pixel = document.querySelectorAll(".canvasContainer > div"); // Only selects the divs inside the canvas container
+    pixel.forEach(pixels => pixels.remove()); // Prevents the canvas to be populated by "invicible" divs
     canvasDisplay.style.gridTemplateRows = `repeat(${pixelSize}, 1fr)`;
     canvasDisplay.style.gridTemplateColumns = `repeat(${pixelSize}, 1fr)`;
 
     let canvasSize = pixelSize * pixelSize;
+    // Populates the canvas container with divs
     for (let i = 0; i < canvasSize; i++) {
-        const pixel = document.createElement("div");
+        let pixel = document.createElement("div");
         pixel.style.border = "1px solid grey";
         canvasDisplay.insertAdjacentElement("beforeend", pixel);
-    }
-}
+    };
+};
 
-makeCanvas(12)
+makeCanvas(16)
+
+// Catch the canvas size input from the user and changes the canvas size using that input
+function changeCanvasSize(size) {
+    size = canvasSizeInput.value;
+    // Limits the user to input only numbers between 2 and 64
+    if (size >= 2 && size <= 64) {
+        return makeCanvas(size)
+    } else {
+        // Fires an alert if the user input is not a valid input
+        messageAlert.textContent = "Size must be a number between 2 and 64 Only. Try again!"
+    }
+};
+
+btnChangeSize.addEventListener("click", changeCanvasSize);
+
+// Cleans the alert from the invalid input
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" || canvasSizeInput.value !== "") return messageAlert.textContent = "";
+})
